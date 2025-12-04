d4cb064006b9dd760ac01fa0d0711b6a9b500f2a
Jegaryan-code <gary.414@gmail.com>
2025-12-04 14:24:47 +0800

    Add files via upload

---
+++ cars.js
@@ -0,0 +1,36 @@
+const CARSPECS = [
+  { image: "SUGO/asurada_gsx.png", acceleration: 4.8, handling: 0.8 },
+  { image: "SUGO/Asurada_GSX_Rally.png", acceleration: 4.8, handling: 0.8 },
+  
+  { image: "SUGO/Super_Asurada_01.png", acceleration: 5.3, handling: 0.7 },
+  { image: "SUGO/Garland.png", acceleration: 4.8, handling: 0.85 },
+  
+  { image: "SUGO/n-Asurada.png", acceleration: 5.3, handling: 0.7 },
+  { image: "SUGO/n-Asurada_AKF-0_G.png", acceleration: 5.3, handling: 0.7 },
+
+  { image: "AOI/Superion_GT.png", acceleration: 4.7, handling: 0.7 },
+  { image: "AOI/Ex-Superion_ZA-8.png", acceleration: 5.1, handling: 0.75 },
+  
+  { image: "AOI/Stealth Jaguar Z-7.png", acceleration: 4.7, handling: 0.7 },
+  { image: "AOI/Stealth Jaguar Z-7B.png", acceleration: 4.7, handling: 0.7 },
+
+  { image: "AOI/Experion_ZA-8_Kaga.png", acceleration: 4.9, handling: 0.9 },
+  { image: "AOI/Ogre.png", acceleration: 5.1, handling: 0.75 },
+  
+  { image: "AOI/Al-Zard NP-1.png", acceleration: 5.1, handling: 0.75 },
+  { image: "AOI/Al-Zard NP-1_B.png", acceleration: 5.1, handling: 0.75 },
+
+  { image: "AOI/EX-zard_ZA-11.png", acceleration: 5.1, handling: 0.75 },
+  { image: "AOI/EX-zard_ZA-11_B.png", acceleration: 5.1, handling: 0.75 },
+  
+  { image: "AOI/Experion_ZA-8_Shoemach.png", acceleration: 5.0, handling: 0.88 },
+  { image: "Union Savior/Knight_Saber.png", acceleration: 4.9, handling: 0.82 },
+  
+  { image: "Union Savior/Issuxark_00-X.png", acceleration: 4.7, handling: 0.8 },
+  { image: "Union Savior/Issuxark_00-X3_II.png", acceleration: 4.7, handling: 0.8 },
+  
+  { image: "STORMZENDER/Spiegel_HP-022.png", acceleration: 4.4, handling: 0.95 }, 
+  { image: "STORMZENDER/Spiegel_HP-022_B.png", acceleration: 4.4, handling: 0.95 }, 
+  
+  { image: "Albatross DDT/Albatrander_602.png", acceleration: 4.7, handling: 0.9 }
+];
