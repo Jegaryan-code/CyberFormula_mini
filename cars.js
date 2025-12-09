b2a31fc56b9862b4d0e529f323dc2becc6cd7b72
Jegaryan-code <gary.414@gmail.com>
2025-12-09 16:52:57 +0800

    Update cars.js

--- cars.js
+++ cars.js
@@ -3,10 +3,11 @@ const CARSPECS = [
   { image: "1. SUGO/Asurada_GSX_Rally.png", acceleration: 4.8, handling: 0.7 },
   
   { image: "1. SUGO/Super_Asurada_01.png", acceleration: 5.1, handling: 0.8 },
-  { image: "1. SUGO/Garland.png", acceleration: 4.9, handling: 0.8 },
-  
   { image: "1. SUGO/n-Asurada.png", acceleration: 5.2, handling: 0.7 },
   { image: "1. SUGO/n-Asurada_AKF-0_G.png", acceleration: 5.3, handling: 0.7 },
+  { image: "1. SUGO/n-Asurada_AKF-0_G_A.png", acceleration: 5.3, handling: 0.7 },
+  
+  { image: "1. SUGO/Garland.png", acceleration: 4.9, handling: 0.8 },
 
   { image: "2. AOI/Superion_GT.png", acceleration: 4.7, handling: 0.7 },
   { image: "2. AOI/Ex-Superion_ZA-8.png", acceleration: 5.1, handling: 0.75 },
@@ -29,10 +30,15 @@ const CARSPECS = [
   { image: "3. UNION SAVIOR/Issuxark_00-X.png", acceleration: 4.7, handling: 0.8 },
   { image: "3. UNION SAVIOR/Issuxark_00-X3_II.png", acceleration: 5.0, handling: 0.8 },
   
-  { image: "4. STORMZENDER/SPIEGEL_HP-022.png", acceleration: 4.6, handling: 0.95 }, 
-  { image: "4. STORMZENDER/SPIEGEL_HP-022_B.png", acceleration: 4.6, handling: 0.95 }, 
+  { image: "4. STORMZENDER/SPIEGEL_HP-022.png", acceleration: 4.7, handling: 0.95 }, 
+  { image: "4. STORMZENDER/SPIEGEL_HP-022_B.png", acceleration: 4.7, handling: 0.95 }, 
   
-  { image: "5. ALBATROSS DDT/Albatrander_602.png", acceleration: 4.5, handling: 0.7 }
+  { image: "5. MISSING LINK/MISSIONNEL_VR-4.png", acceleration: 4.6, handling: 0.7 },  
+  { image: "5. MISSING LINK/STRATMISSIONNEL_MS-3.png", acceleration: 4.7, handling: 0.8 },  
+  
+  { image: "6. OTHERS SINGLE RACER/Albatrander_602.png", acceleration: 4.5, handling: 0.7 },
+  { image: "6. OTHERS SINGLE RACER/EL-CONDOL_B-15.png", acceleration: 4.5, handling: 0.7 },
+  { image: "6. OTHERS SINGLE RACER/SILENT_SCREAMER-β.png", acceleration: 4.5, handling: 0.7 }
 ];
 
 
