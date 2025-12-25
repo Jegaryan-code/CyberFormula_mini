ea4b9ecae7edcef1eac9666be2dcf47f587595fc
Jegaryan-code <gary.414@gmail.com>
2025-12-25 14:30:46 +0800

    Update cars.js

--- cars.js
+++ cars.js
@@ -4,21 +4,22 @@ const CARSPECS = [
   
   { image: "1. SUGO/Super_Asurada_01.png", acceleration: 5.1, handling: 0.8 },
   { image: "1. SUGO/n-Asurada.png", acceleration: 5.2, handling: 0.7 },
+  { image: "1. SUGO/n-Asurada-0.png", acceleration: 5.25, handling: 0.7 },
   { image: "1. SUGO/n-Asurada_AKF-0_G.png", acceleration: 5.3, handling: 0.7 },
   { image: "1. SUGO/n-Asurada_AKF-0_G_A.png", acceleration: 5.3, handling: 0.7 },
   
-  { image: "1. SUGO/Garland.png", acceleration: 4.9, handling: 0.8 },
+  { image: "1. SUGO/Garland.png", acceleration: 5.0, handling: 0.8 },
 
-  { image: "2. AOI/Superion_GT.png", acceleration: 4.7, handling: 0.7 },
+  { image: "2. AOI/Superion_GT.png", acceleration: 4.8, handling: 0.7 },
   { image: "2. AOI/Ex-Superion_ZA-8.png", acceleration: 5.1, handling: 0.75 },
   
   { image: "2. AOI/Stealth Jaguar Z-7.png", acceleration: 4.7, handling: 0.7 },
   { image: "2. AOI/Stealth Jaguar Z-7B.png", acceleration: 4.75, handling: 0.75 },
 
   { image: "2. AOI/Experion_ZA-8_Kaga.png", acceleration: 4.9, handling: 0.8 },
-  { image: "2. AOI/Ogre.png", acceleration: 5.3, handling: 0.95 },
+  { image: "2. AOI/Ogre.png", acceleration: 5.35, handling: 0.95 },
   
-  { image: "2. AOI/Al-Zard NP-1.png", acceleration: 5.1, handling: 0.75 },
+  { image: "2. AOI/Al-Zard NP-1.png", acceleration: 5.2, handling: 0.75 },
   { image: "2. AOI/Al-Zard NP-1_B.png", acceleration: 5.1, handling: 0.75 },
 
   { image: "2. AOI/EX-zard_ZA-11.png", acceleration: 5.3, handling: 0.8 },
@@ -29,16 +30,16 @@ const CARSPECS = [
   
   { image: "3. UNION SAVIOR/Issuxark_00-X.png", acceleration: 4.7, handling: 0.8 },
   { image: "3. UNION SAVIOR/Issuxark_00-X3_II.png", acceleration: 5.0, handling: 0.8 },
+  { image: "3. UNION SAVIOR/Issuxark_00-X3_II_B.png", acceleration: 5.1, handling: 0.8 },
   
-  { image: "4. STORMZENDER/SPIEGEL_HP-022.png", acceleration: 4.7, handling: 0.95 }, 
-  { image: "4. STORMZENDER/SPIEGEL_HP-022_B.png", acceleration: 4.7, handling: 0.95 }, 
+  { image: "4. STORMZENDER/SPIEGEL_HP-022.png", acceleration: 4.9, handling: 0.95 }, 
+  { image: "4. STORMZENDER/SPIEGEL_HP-022_2.png", acceleration: 4.95, handling: 0.95 },  
+  { image: "4. STORMZENDER/SPIEGEL_HP-022_B.png", acceleration: 4.8, handling: 0.95 }, 
   
   { image: "5. MISSING LINK/MISSIONNEL_VR-4.png", acceleration: 4.6, handling: 0.7 },  
   { image: "5. MISSING LINK/STRATMISSIONNEL_MS-3.png", acceleration: 4.7, handling: 0.8 },  
   
-  { image: "6. OTHERS SINGLE RACER/Albatrander_602.png", acceleration: 4.5, handling: 0.7 },
+  { image: "6. OTHERS SINGLE RACER/Albatrander_602.png", acceleration: 4.6, handling: 0.7 },
   { image: "6. OTHERS SINGLE RACER/EL-CONDOL_B-15.png", acceleration: 4.5, handling: 0.7 },
-  { image: "6. OTHERS SINGLE RACER/SILENT_SCREAMER-β.png", acceleration: 4.5, handling: 0.7 }
+  { image: "6. OTHERS SINGLE RACER/SILENT_SCREAMER-β.png", acceleration: 4.6, handling: 0.7 }
 ];
-
-
