b31eda10f68005b4ea545f908d1fb165caf3f84a
Jegaryan-code <gary.414@gmail.com>
2026-01-02 16:15:52 +0800

    Update cars.js

--- cars.js
+++ cars.js
@@ -48,8 +48,9 @@ const CARSPECS = [
   { image: "6. OTHERS SINGLE RACER/EL-CONDOL_B-15.png", acceleration: 4.5, handling: 0.7 },
   { image: "6. OTHERS SINGLE RACER/SILENT_SCREAMER-β.png", acceleration: 4.6, handling: 0.7 },
   { image: "6. OTHERS SINGLE RACER/Stampede_RS.png", acceleration: 4.7, handling: 0.7 },
-  { image: "6. OTHERS SINGLE RACER/Advance_Phoenix.png", acceleration: 5.3, handling: 0.8 }
+  { image: "6. OTHERS SINGLE RACER/Advanced_Phoenix.png", acceleration: 5.3, handling: 0.8 }
 ];
+
 
 
 
