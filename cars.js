2825bcf73c79ebd511633b3c1ea5098b88c0e43f
Jegaryan-code <gary.414@gmail.com>
2026-01-08 12:37:29 +0800

    Update cars.js

--- cars.js
+++ cars.js
@@ -1,60 +1,59 @@
 const CARSPECS = [
-  { image: "1. SUGO/asurada_gsx.png", acceleration: 4.8, handling: 0.7 },
-  { image: "1. SUGO/Asurada_GSX_Rally.png", acceleration: 4.8, handling: 0.7 },
+  { image: "1. SUGO/asurada_gsx.png", acceleration: 4.8, handling: 0.7, hasAero: false, hasBoost: true},
+  { image: "1. SUGO/Asurada_GSX_Rally.png", acceleration: 4.8, handling: 0.7 , hasAero: false, hasBoost: true},
   
-  { image: "1. SUGO/Super_Asurada_01.png", acceleration: 5.1, handling: 0.8 },
-  { image: "1. SUGO/S-Asurada-AKF-11.png", acceleration: 5.25, handling: 0.7 },
-  { image: "1. SUGO/n-Asurada_AKF-0_G.png", acceleration: 5.3, handling: 0.7 },
-  { image: "1. SUGO/n-Asurada_AKF-0_G_A.png", acceleration: 5.3, handling: 0.7 },
-  { image: "1. SUGO/Vision-Asurada.png", acceleration: 5.2, handling: 0.7 },
+  { image: "1. SUGO/Super_Asurada_01.png", acceleration: 5.1, handling: 0.8 , hasAero: true, hasBoost: true},
+  { image: "1. SUGO/S-Asurada-AKF-11.png", acceleration: 5.25, handling: 0.7 , hasAero: true, hasBoost: true},
+  { image: "1. SUGO/n-Asurada_AKF-0_G.png", acceleration: 5.3, handling: 0.7 , hasAero: true, hasBoost: true},
+  { image: "1. SUGO/n-Asurada_AKF-0_G_A.png", acceleration: 5.3, handling: 0.7 , hasAero: true, hasBoost: true},
+  { image: "1. SUGO/Vision-Asurada.png", acceleration: 5.2, handling: 0.7 , hasAero: true, hasBoost: true},
   
-  { image: "1. SUGO/Garland_Kazami.png", acceleration: 5.25, handling: 0.8 },
-  { image: "1. SUGO/Garland_Henri.png", acceleration: 5.2, handling: 0.8 },
+  { image: "1. SUGO/Garland_Kazami.png", acceleration: 5.25, handling: 0.8 , hasAero: false, hasBoost: false},
+  { image: "1. SUGO/Garland_Henri.png", acceleration: 5.2, handling: 0.8 , hasAero: false, hasBoost: false},
 
-  { image: "2. AOI/Superion_GT.png", acceleration: 4.8, handling: 0.7 },
-  { image: "2. AOI/Ex-Superion_ZA-8_Shinjo.png", acceleration: 5.1, handling: 0.75 },
+  { image: "2. AOI/Superion_GT.png", acceleration: 4.8, handling: 0.7 , hasAero: false, hasBoost: true},
+  { image: "2. AOI/Ex-Superion_ZA-8_Shinjo.png", acceleration: 5.1, handling: 0.75 , hasAero: true, hasBoost: true},
   
-  { image: "2. AOI/Stealth Jaguar Z-7.png", acceleration: 4.7, handling: 0.7 },
-  { image: "2. AOI/Stealth Jaguar Z-7B.png", acceleration: 4.75, handling: 0.75 },
+  { image: "2. AOI/Stealth Jaguar Z-7.png", acceleration: 4.7, handling: 0.7 , hasAero: false, hasBoost: false},
+  { image: "2. AOI/Stealth Jaguar Z-7B.png", acceleration: 4.75, handling: 0.75 , hasAero: false, hasBoost: false},
 
-  { image: "2. AOI/Experion_ZA-8_Kaga.png", acceleration: 4.9, handling: 0.8 },
-  { image: "2. AOI/Ogre.png", acceleration: 5.35, handling: 0.95 },
+  { image: "2. AOI/Experion_ZA-8_Kaga.png", acceleration: 4.9, handling: 0.8 , hasAero: true, hasBoost: true},
+  { image: "2. AOI/Ogre.png", acceleration: 5.35, handling: 0.95 , hasAero: true, hasBoost: true},
   
-  { image: "2. AOI/Al-Zard NP-1.png", acceleration: 5.2, handling: 0.75 },
-  { image: "2. AOI/Al-Zard NP-1_B.png", acceleration: 5.1, handling: 0.75 },
-  { image: "2. AOI/Al-Zard NP-1_Kaga.png", acceleration: 5.1, handling: 0.75 },  
-  { image: "2. AOI/Al-Zard NP-2.png", acceleration: 5.25, handling: 0.75 },
+  { image: "2. AOI/Al-Zard NP-1.png", acceleration: 5.2, handling: 0.75 , hasAero: true, hasBoost: true},
+  { image: "2. AOI/Al-Zard NP-1_B.png", acceleration: 5.1, handling: 0.75 , hasAero: true, hasBoost: true},
+  { image: "2. AOI/Al-Zard NP-1_Kaga.png", acceleration: 5.1, handling: 0.75 , hasAero: true, hasBoost: true},  
+  { image: "2. AOI/Al-Zard NP-2.png", acceleration: 5.25, handling: 0.75 , hasAero: true, hasBoost: true},
   
-  { image: "2. AOI/EX-zard_ZA-11_Kaga.png", acceleration: 5.3, handling: 0.8 },
-  { image: "2. AOI/EX-zard_ZA-11_Shiba.png", acceleration: 5.3, handling: 0.8 },
-  { image: "2. AOI/EX-zard_ZA-11_B.png", acceleration: 5.3, handling: 0.8 },
+  { image: "2. AOI/EX-zard_ZA-11_Kaga.png", acceleration: 5.3, handling: 0.8 , hasAero: true, hasBoost: true},
+  { image: "2. AOI/EX-zard_ZA-11_Shiba.png", acceleration: 5.3, handling: 0.8 , hasAero: true, hasBoost: true},
+  { image: "2. AOI/EX-zard_ZA-11_B.png", acceleration: 5.3, handling: 0.8 , hasAero: true, hasBoost: true},
   
-  { image: "2. AOI/Experion_ZA-8_Shoemach.png", acceleration: 5.0, handling: 0.88 },
-  { image: "3. UNION SAVIOR/Knight_Saber.png", acceleration: 4.9, handling: 0.82 },
+  { image: "2. AOI/Experion_ZA-8_Shoemach.png", acceleration: 5.0, handling: 0.88 , hasAero: true, hasBoost: true},
+  { image: "3. UNION SAVIOR/Knight_Saber.png", acceleration: 4.9, handling: 0.82 , hasAero: false, hasBoost: false},
   
-  { image: "3. UNION SAVIOR/Issuxark_00-X.png", acceleration: 4.7, handling: 0.8 },
-  { image: "3. UNION SAVIOR/Issuxark_00-X3_II_Randoll.png", acceleration: 5.0, handling: 0.8 },
-  { image: "3. UNION SAVIOR/Issuxark_00-X3_II_Shinjo.png", acceleration: 5.1, handling: 0.8 },
+  { image: "3. UNION SAVIOR/Issuxark_00-X.png", acceleration: 4.7, handling: 0.8 , hasAero: false, hasBoost: true},
+  { image: "3. UNION SAVIOR/Issuxark_00-X3_II_Randoll.png", acceleration: 5.0, handling: 0.8 , hasAero: false, hasBoost: true},
+  { image: "3. UNION SAVIOR/Issuxark_00-X3_II_Shinjo.png", acceleration: 5.1, handling: 0.8 , hasAero: false, hasBoost: true},
   
-  { image: "4. STORMZENDER/Stil_HG-161.png", acceleration: 4.8, handling: 0.95 },
-  { image: "4. STORMZENDER/Stil_HG-162.png", acceleration: 4.8, handling: 0.95 },
-  { image: "4. STORMZENDER/Stil_HG-164.png", acceleration: 4.9, handling: 0.95 },
-  { image: "4. STORMZENDER/Stil_HG-165.png", acceleration: 4.9, handling: 0.95 },
-  { image: "4. STORMZENDER/SPIEGEL_HP-022.png", acceleration: 5.0, handling: 0.95 }, 
-  { image: "4. STORMZENDER/SPIEGEL_HP-022_2.png", acceleration: 5.05, handling: 0.95 },  
-  { image: "4. STORMZENDER/SPIEGEL_HP-022_B.png", acceleration: 5.0, handling: 0.95 }, 
+  { image: "4. STORMZENDER/Stil_HG-161.png", acceleration: 4.8, handling: 0.95 , hasAero: false, hasBoost: false},
+  { image: "4. STORMZENDER/Stil_HG-162.png", acceleration: 4.8, handling: 0.95 , hasAero: false, hasBoost: false},
+  { image: "4. STORMZENDER/Stil_HG-164.png", acceleration: 4.9, handling: 0.95 , hasAero: false, hasBoost: false},
+  { image: "4. STORMZENDER/Stil_HG-165.png", acceleration: 4.9, handling: 0.95 , hasAero: false, hasBoost: false},
+  { image: "4. STORMZENDER/SPIEGEL_HP-022.png", acceleration: 5.0, handling: 0.95 , hasAero: true, hasBoost: false}, 
+  { image: "4. STORMZENDER/SPIEGEL_HP-022_2.png", acceleration: 5.05, handling: 0.95 , hasAero: true, hasBoost: false},  
+  { image: "4. STORMZENDER/SPIEGEL_HP-022_B.png", acceleration: 5.0, handling: 0.95 , hasAero: false, hasBoost: false}, 
   
-  { image: "5. MISSING LINK/MISSIONNEL_VR-4.png", acceleration: 4.7, handling: 0.7 },  
-  { image: "5. MISSING LINK/STRATMISSIONNEL_MS-3.png", acceleration: 4.85, handling: 0.8 },
-  { image: "5. MISSING LINK/Asurada_GST.png", acceleration: 4.8, handling: 0.75 }, 
+  { image: "5. MISSING LINK/MISSIONNEL_VR-4.png", acceleration: 4.7, handling: 0.7 , hasAero: false, hasBoost: false},  
+  { image: "5. MISSING LINK/STRATMISSIONNEL_MS-3.png", acceleration: 4.8, handling: 0.8 , hasAero: false, hasBoost: false},
+  { image: "5. MISSING LINK/Asurada_GST.png", acceleration: 4.8, handling: 0.8 , hasAero: false, hasBoost: false},  
   
-  { image: "6. OTHERS SINGLE RACER/Albatrander_602.png", acceleration: 4.6, handling: 0.7 },
-  { image: "6. OTHERS SINGLE RACER/EL-CONDOL_B-15.png", acceleration: 4.5, handling: 0.7 },
-  { image: "6. OTHERS SINGLE RACER/SILENT_SCREAMER-β.png", acceleration: 4.6, handling: 0.7 },
-  { image: "6. OTHERS SINGLE RACER/Stampede_RS.png", acceleration: 4.7, handling: 0.7 },
-  { image: "6. OTHERS SINGLE RACER/Advanced_Phoenix.png", acceleration: 5.3, handling: 0.8 }
+  { image: "6. OTHERS SINGLE RACER/Albatrander_602.png", acceleration: 4.6, handling: 0.7 , hasAero: false, hasBoost: false},
+  { image: "6. OTHERS SINGLE RACER/EL-CONDOL_B-15.png", acceleration: 4.5, handling: 0.7 , hasAero: false, hasBoost: false},
+  { image: "6. OTHERS SINGLE RACER/SILENT_SCREAMER-β.png", acceleration: 4.6, handling: 0.7 , hasAero: false, hasBoost: false},
+  { image: "6. OTHERS SINGLE RACER/Stampede_RS.png", acceleration: 4.7, handling: 0.7 , hasAero: false, hasBoost: false},
+  { image: "6. OTHERS SINGLE RACER/Advanced_Phoenix.png", acceleration: 5.3, handling: 0.8 , hasAero: true, hasBoost: true}
 ];
-
 
 
 
